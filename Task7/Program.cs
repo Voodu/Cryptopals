6087995e6ba54042563ad1a787926503198d45b6
Piotr Ładoński <piolado@gmail.com>
2018-10-21 00:23:41 +0300

    Implemented using builtin library

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using Utilities;
 
 namespace Task7
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cipherText = new B64(File.ReadAllText(@"D:\VS17 projects\Cryptopals\Task7\7.txt")).GetBytes();
+            var key = new Readable("YELLOW SUBMARINE").GetASCIIBytes();
+
+            using (var aes = new AesManaged())
+            {
+                aes.Key = key;
+                string plaintext = null;
+                aes.Mode = CipherMode.ECB;
+                aes.Padding = PaddingMode.Zeros;
+
+                // Create a decryptor to perform the stream transform.
+                var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                // Create the streams used for decryption.
+                using (var msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+                Console.WriteLine(plaintext);
+            }
         }
     }
 }
