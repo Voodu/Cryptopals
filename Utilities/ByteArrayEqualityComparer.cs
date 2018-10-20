4cac4890b2fe4752ad76a9172bdecd6a3c21028e
Piotr Ładoński <piolado@gmail.com>
2018-10-21 02:02:29 +0300

    Implemented task8

---
+++ Utilities/ByteArrayEqualityComparer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Utilities
+{
+    public class ByteArrayEqualityComparer : IEqualityComparer<byte[]>
+    {
+        public bool Equals(byte[] x, byte[] y)
+        {
+            if (x == null || y == null || x.Length != y.Length)
+            {
+                return false;
+            }
+
+            for (var i = 0; i < x.Length; i++)
+            {
+                if (x[i] != y[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public int GetHashCode(byte[] obj)
+        {
+            unchecked
+            {
+                const int p = 16777619;
+                var hash = obj.Aggregate((int) 2166136261, (current, t) => (current ^ t) * p);
+
+                hash += hash << 13;
+                hash ^= hash >> 7;
+                hash += hash << 3;
+                hash ^= hash >> 17;
+                hash += hash << 5;
+                return hash;
+            }
+        }
+    }
+}
